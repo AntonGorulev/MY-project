5c6aa28063786c13ac920966c93379c28c453b86
Anton <gorulev2020@inbox.ru>
2021-04-02 01:27:51 +0300

    add templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from .models import Material
 # Create your views here.
+
+
+def home(request):
+
+    list_material = Material.objects.all()
+    context ={
+        'name':'Anton',
+        'list_material':list_material
+    }
+
+    template ='index.html'
+
+    return render(request, template, context)
+
+def detail(request, id):
+
+    get_material = Material.objects.get(id=id)
+
+    context = {
+        'get_material':get_material
+    }
+
+    template = 'detail.html'
+
+    return render(request, template, context)
