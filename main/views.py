5fb666622120b9be01c00efe394dee9c7b00f635
Anton <gorulev2020@inbox.ru>
2021-05-01 14:44:47 +0300

    authorization and registration

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Material
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .forms import ArticleForm
+from .forms import ArticleForm, AuthUserForm, RegisterUserForm
 from django.urls import reverse, reverse_lazy
 from django.contrib import messages
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 class HomeListView(ListView):
     model = Material
@@ -48,7 +50,37 @@ class ArticleUpdateView(UpdateView):
         kwargs['update'] = True
         return super().get_context_data(**kwargs)
 
+class MyprojectLoginView(LoginView):
+    template_name = 'login.html'
+    form_class = AuthUserForm
+    success_url = reverse_lazy('edit_page')
+    def get_success_url(self):
+        return self.success_url
+
+class RegisterUserView(CreateView):
+    model = User
+    template_name = 'register_page.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('edit_page')
+    success_msg = 'Пользователь успешно создан'
+    def form_valid(self,form):
+        form_valid = super().form_valid(form)
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+        aut_user = authenticate(username=username,password=password)
+        login(self.request, aut_user)
+        return form_valid    
+
+class MyProjectLogout(LogoutView):
+    next_page = reverse_lazy('edit_page')
+
 class ArticleDeleteView(DeleteView):
     model = Material
     template_name = 'edit_page.html'
     success_url = reverse_lazy('edit_page')
+    success_msg = 'Запись удалена'
+
+    def post(self,request,*args,**kwargs):
+        messages.success(self.request, self.success_msg)
+        return super().post(request)
+    
