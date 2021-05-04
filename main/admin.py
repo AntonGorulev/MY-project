449b1da3db62fccdb58b9f54a3b057defb97eae4
Anton <gorulev2020@inbox.ru>
2021-05-04 21:07:52 +0300

    admin setting

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from. import models
 
 
-admin.site.register(models.Material)
+#admin.site.register(models.Material)
+@admin.register(models.Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('author', 'name', 'create_date')
+    list_filter = ('create_date', 'text')
+    search_fields = ('name', 'text')
